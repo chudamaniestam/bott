f34e993c70bc31160c3a97f65d5f75e6d487afab
chudamaniestam <chudamaniestam@gmail.com>
2018-06-16 21:08:29 +0530

    iam created heroku

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express=require('express');
 var app=express();
 var port = process.env.PORT||3001;
-app.get('/endpoint',function(req,res){
-res.send('endpoint creation');});
+
+app.post('/endpoint',function(req,res){
+	console.log(request.body);
+	res.send('endpoint creation');
+});
+
+
 var server=app.listen(port,function(){
-console.log('server started');
+	console.log('server started');
 });
